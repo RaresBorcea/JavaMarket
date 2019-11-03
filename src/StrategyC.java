5582d5ed0e59251378e225695ca3a8f6770e4795
Rares Borcea <raresborcea@gmail.com>
2019-11-03 16:24:20 +0200

    Update StrategyC.java

--- src/StrategyC.java
+++ src/StrategyC.java
@@ -1,7 +1,7 @@
 
 public class StrategyC implements Strategy {
-	//comparators were used so that the needed element
-	//is always on the first position
+	// comparators were used so that the needed element
+	// is always on the first position
 	public Item execute(WishList wishList) {
 		return (Item)wishList.remove(0);
 	}
